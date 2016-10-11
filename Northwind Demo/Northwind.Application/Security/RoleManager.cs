361c0987dd5ba0a31d57f384616e50b93309a4a0
Dan Gilleland <dgilleland@nait.ca>
2016-10-11 11:17:49 -0600

    Set up default roles

--- Northwind Demo/Northwind.Application/Security/RoleManager.cs
+++ Northwind Demo/Northwind.Application/Security/RoleManager.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Linq;
 
 namespace Northwind.Application.Security
 {
@@ -8,6 +9,18 @@ namespace Northwind.Application.Security
         public RoleManager()
             : base(new RoleStore<IdentityRole>(new ApplicationDbContext()))
         {
+        }
+
+        public void AddDefaultRoles()
+        {
+            foreach (string roleName in SecurityRoles.DefaultSecurityRoles)
+            {
+                // Check if it exists
+                if (!Roles.Any(r => r.Name == roleName))
+                {
+                    this.Create(new IdentityRole(roleName));
+                }
+            }
         }
     }
 }
