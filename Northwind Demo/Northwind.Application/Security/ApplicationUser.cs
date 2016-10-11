da539ea0952a9aa7553568374c6e7a6ab878d211
Dan Gilleland <dgilleland@nait.ca>
2016-10-11 11:12:48 -0600

    Add foreign key properties to ApplicationUser
    No actual foreign key constraints. An ApplicationUser (user who can log
    in) is either an employee or a customer.

--- Northwind Demo/Northwind.Application/Security/ApplicationUser.cs
+++ Northwind Demo/Northwind.Application/Security/ApplicationUser.cs
@@ -5,5 +5,7 @@ namespace Northwind.Application.Security
     // You can add User data for the user by adding more properties to your User class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public int? EmployeeId { get; set; }
+        public string CustomerId { get; set; }
     }
 }
