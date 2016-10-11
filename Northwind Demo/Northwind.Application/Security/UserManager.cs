016c93cb754dec5a2647ae025aa8bdd5d2c4f12b
Dan Gilleland <dgilleland@nait.ca>
2016-10-11 11:32:28 -0600

    Setup WebMaster user
    Including the webmaster username and password in the About.aspx.

--- Northwind Demo/Northwind.Application/Security/UserManager.cs
+++ Northwind Demo/Northwind.Application/Security/UserManager.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Linq;
 
 namespace Northwind.Application.Security
 {
@@ -8,6 +9,29 @@ namespace Northwind.Application.Security
         public UserManager()
             : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
         {
+        }
+
+        #region Constants
+        private const string STR_DEFAULT_PASSWORD = "Pa$$word1";
+        /// <summary>Requires FirstName and LastName</summary>
+        private const string STR_USERNAME_FORMAT = "{0}.{0}";
+        /// <summary>Requires UserName</summary>
+        private const string STR_EMAIL_FORMAT = "{0}@Northwind.tba";
+        private const string STR_WEBMASTER_USERNAME = "Webmaster";
+        #endregion
+
+        public void AddWebMaster()
+        {
+            // Add a web  master user
+            if (!Users.Any(u => u.UserName.Equals(STR_WEBMASTER_USERNAME)))
+            {
+                var webMasterAccount = new ApplicationUser()
+                {
+                    UserName = STR_WEBMASTER_USERNAME,
+                    Email = string.Format(STR_EMAIL_FORMAT, STR_WEBMASTER_USERNAME)
+                };
+                this.Create(webMasterAccount, STR_DEFAULT_PASSWORD);
+            }
         }
     }
 }
--- Northwind Demo/Website/Global.asax
+++ Northwind Demo/Website/Global.asax
@@ -2,6 +2,7 @@
 <%@ Import Namespace="Website" %>
 <%@ Import Namespace="System.Web.Optimization" %>
 <%@ Import Namespace="System.Web.Routing" %>
+<%@ Import Namespace="Northwind.Application.Security" %>
 
 <script runat="server">
 
@@ -9,6 +10,10 @@
     {
         RouteConfig.RegisterRoutes(RouteTable.Routes);
         BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+        // Ensure there is a Webmaster account
+        var UserManager = new UserManager();
+        UserManager.AddWebMaster();
     }
 
 </script>
