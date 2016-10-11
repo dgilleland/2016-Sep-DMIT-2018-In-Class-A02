c54ce197c8df8b0da40e29fd82b166121e4dd1b8
Dan Gilleland <dgilleland@nait.ca>
2016-10-11 11:06:24 -0600

    Add RoleManager class

---
+++ Northwind Demo/Northwind.Application/Security/RoleManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Northwind.Application.Security
+{
+    public class RoleManager : RoleManager<IdentityRole>
+    {
+        public RoleManager()
+            : base(new RoleStore<IdentityRole>(new ApplicationDbContext()))
+        {
+        }
+    }
+}
