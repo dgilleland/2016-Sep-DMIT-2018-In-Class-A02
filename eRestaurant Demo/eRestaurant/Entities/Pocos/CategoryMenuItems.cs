c4d71cb1d3e4632cbac40f9e1bdd3d7d84948de9
Dan Gilleland <dgilleland@nait.ca>
2016-11-15 15:30:20 -0700

    POCO for my report

---
+++ eRestaurant Demo/eRestaurant/Entities/Pocos/CategoryMenuItems.cs
@@ -0,0 +1,11 @@
+﻿namespace eRestaurant.Entities.Pocos
+{
+    public class CategoryMenuItems
+    {
+        public string CategoryDescription { get; set; }
+        public string ItemDescription { get; set; }
+        public decimal Price { get; set; }
+        public int? Calories { get; set; }
+        public string Comment { get; set; }
+    }
+}
