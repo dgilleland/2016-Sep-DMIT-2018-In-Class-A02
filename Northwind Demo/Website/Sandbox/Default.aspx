d8390e89934fe2f993fb4bf4ce59938310aa8d24
Dan Gilleland <dgilleland@nait.ca>
2016-12-05 13:20:06 -0700

    Logged in username - Sandbox sample

---
+++ Northwind Demo/Website/Sandbox/Default.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Sandbox_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="Server">
+    <h1 class="page-header">Sandbox</h1>
+    <div class="row">
+        <div class="col-md-12">
+            <asp:Label ID="UserNameLabel" runat="server" />
+        </div>
+    </div>
+</asp:Content>
+
---
+++ Northwind Demo/Website/Sandbox/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Sandbox_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(Request.IsAuthenticated)
+        {
+            UserNameLabel.Text = "You are logged in as '" + User.Identity.Name + "'";
+        }
+        else
+        {
+            UserNameLabel.Text = "You are not logged into the website.";
+        }
+    }
+}
