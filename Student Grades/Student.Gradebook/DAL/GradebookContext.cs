925e3b1438af2512427a4b372c4641fd05bcd4c7
Dan Gilleland <dgilleland@nait.ca>
2016-12-08 15:12:23 -0700

    Student.Gradebook class library

---
+++ Student Grades/Student.Gradebook/DAL/GradebookContext.cs
@@ -0,0 +1,21 @@
+﻿using Student.Gradebook.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student.Gradebook.DAL
+{
+    internal class GradebookContext : DbContext
+    {
+        public GradebookContext() : base("DefaultConnection") { }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseOffering> CourseOfferings { get; set; }
+        public DbSet<CourseSection> CourseSections { get; set; }
+        public DbSet<ClassMember> ClassMembers { get; set; }
+        public DbSet<Entities.Student> Students { get; set; }
+    }
+}
---
+++ Student Grades/Student.Gradebook/Entities/Course.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student.Gradebook.Entities
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+
+        public virtual ICollection<CourseOffering> Offerings { get; set; }
+
+        public Course()
+        {
+            Offerings = new HashSet<CourseOffering>();
+        }
+    }
+
+    public enum TermStart { Other, Sep, Jan, May }
+
+    public class CourseOffering
+    {
+        public int CourseOfferingId { get; set; }
+
+        public int CourseId { get; set; }
+        public int Year { get; set; }
+        public TermStart Term { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual ICollection<CourseSection> Sections { get; set; }
+
+        public CourseOffering()
+        {
+            Sections = new HashSet<CourseSection>();
+        }
+    }
+
+    public class CourseSection
+    {
+        public int CourseSectionId { get; set; }
+        public int CourseOfferingId { get; set; }
+        public string Name { get; set; }
+
+        public virtual CourseOffering CourseOffering { get; set; }
+        public virtual ICollection<ClassMember> Students { get; set; }
+
+        public CourseSection()
+        {
+            Students = new HashSet<ClassMember>();
+        }
+    }
+
+    public class ClassMember
+    {
+        [Key,Column(Order = 1)]
+        public int CourseSectionId { get; set; }
+        [Key, Column(Order = 2)]
+        public int StudentId { get; set; }
+
+        public double? Mark { get; set; }
+
+        public virtual CourseSection CourseSection { get; set; }
+        public virtual Student Student { get; set; }
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string SchoolId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual ICollection<ClassMember> Classes { get; set; }
+    }
+
+}
